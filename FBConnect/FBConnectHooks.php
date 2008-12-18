01a420f4d1a268085f4595a146f54d4c95c2e45a
garbear <garbearucla@gmail.com>
2008-12-18 21:53:54 +0000

    Implemented using XFBML tags in wiki text

--- FBConnect/FBConnectHooks.php
+++ FBConnect/FBConnectHooks.php
@@ -30,7 +30,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  * This class contains all the hooks used in this extension. HOOKS DO NOT NEED
  * TO BE EXPLICITLY ADDED TO $wgHooks. Simply write a function with the same
  * name as the hook that initiates it, place it inside this class and let
- * setup() do its magic.
+ * FBConnect::setup() do its magic.
  */
 class FBConnectHooks {
 	/**
@@ -41,6 +41,167 @@ class FBConnectHooks {
 		return true;
 	}
 	
+	/**
+	 * Injects some CSS and Javascript into the <head> of the page
+	 */
+	static function BeforePageDisplay(&$out, &$sk) {
+		global $wgTitle, $wgFBConnectLogoUrl, $wgScriptPath;
+		$thisurl = $wgTitle->getPrefixedURL();
+		
+		// Add a pretty Facebook logo in front of the userpage's link
+		$style = '<style type="text/css">
+			li#pt-userpage {
+				background: url(' . $wgFBConnectLogoUrl . ') top left no-repeat;
+			}
+		</style>';
+		
+		// Setup some variables and pseudo window.onload functions for Facebook Connect
+		$script = "";
+		$js_vars = array(
+			'api_key' => FBConnect::get_api_key(),
+			'already_logged_into_facebook' => FBConnect::getClient()->get_loggedin_user() ? "true" : "false",
+			'logout_url' => Skin::makeSpecialUrl('Userlogout', $wgTitle->isSpecial('Preferences') ? '' : "returnto={$thisurl}")
+		);
+		foreach( $js_vars as $name => $value ) {
+			if( $value == "true" || $value == "false" ) {
+				$script .= "var " . $name . " = " . $value . ";\n";
+			} else {
+				$script .= "var " . $name . " = '" . $value . "';\n";
+			}
+		}
+		// Onload functions from fbconnect.js (actually called in the <body> by addOnloadHook())
+		foreach( array( 'facebook_onload_addFBConnectButtons', 'facebook_init', 'facebook_onload' ) as $hook ) {
+			$script .= "addOnloadHook($hook);\n";
+		}
+		
+		// Styles and Scripts have been built, so add them to the page
+		if (isset($wgFBConnectLogoUrl) && $wgFBConnectLogoUrl) {
+			$out->addScript($style . "\n\t\t");
+		}
+		$out->addScript("<script src='$wgScriptPath/extensions/FBConnect/fbconnect.js'></script>");
+		$out->addInlineScript($script);
+		return true;
+	}
+	
+	/**
+	 * We seriously need to use a better hook... But which one allows injecting javascript src's into the page's body?
+	 * The dynamic source code loading [newElement("source") ...] technique didn't work for me.
+	 *
+	 */
+	static function ParserAfterTidy(&$parser, &$text) {
+		static $wgOnce = false;
+		//if (!isset($wgOnce) || !$wgOnce) {
+		if (!$wgOnce) {
+			$wgOnce = true;
+			self::SomeHookThatAllowsOneTimeRenderingToFooter($text);
+		}
+		return true;
+	}
+	
+	/**
+	 * Is there any hook for this task?
+	 *
+	 * Perhaps one of the skin hooks: SkinAfterBottomScripts, SkinAfterContent or SkinBuildSidebar...
+	 * Found one: SiteNoticeAfter
+	 */
+	static function SomeHookThatAllowsOneTimeRenderingToFooter(&$text) {
+		$text .= '<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php"></script>';
+		//$text .= '<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/XdCommReceiver.js" type="text/javascript"></script>';
+		//$text .= '<script src="/w/extensions/FBConnect/fbconnect.js"></script>';
+		return true;
+	}
+
+	/**
+	 * Installs a parser hook for every tag reported by FBConnectXFBML::availableTags().
+	 * Accomplishes this by asking FBConnectXFBML to create a hook function that then
+	 * redirects to FBConnectXFBML::parserHook().
+	 */
+	static function ParserFirstCallInit(&$parser) {
+		$pHooks = FBConnectXFBML::availableTags();
+		foreach( $pHooks as $tag ) {
+			$parser->setHook( $tag, FBConnectXFBML::createParserHook($tag) );
+		}
+		return true;
+	}
+	
+	/**
+	 * Modify the user's persinal toolbar (in the upper right)
+	 */
+	static function PersonalUrls(&$personal_urls, &$title) {
+		global $wgUser, $wgLang, $wgOut, $wgFBConnectOnly;
+		wfLoadExtensionMessages('FBConnect');
+		$sk = $wgUser->getSkin();
+		
+		if ( !$wgUser->isLoggedIn() ) {
+			$returnto = ($title->getPrefixedUrl() == $wgLang->specialPage( 'Userlogout' )) ?
+			  '' : ('returnto=' . $title->getPrefixedURL());
+
+			$personal_urls['fbconnect'] = array('text' => wfMsg('fbconnectlogin'),
+			                                    #'href' => $sk->makeSpecialUrl( 'Userlogin', $returnto ),
+			                                    'href' => $sk->makeSpecialUrl( 'Connect', $returnto ),
+			                                    'active' => $title->isSpecial( 'Userlogin' ) );
+			if ($wgFBConnectOnly) {
+				# remove other personal toolbar links
+				foreach (array('login', 'anonlogin') as $k) {
+					if (array_key_exists($k, $personal_urls)) {
+						unset($personal_urls[$k]);
+					}
+				}
+			}
+		} else {
+			/* User's real name is not set at account creation. Why not? And why doesn't this workaround seem to work?
+			if ($wgUser->getRealName() == "") {
+				$wgAuth->updateUser($wgUser);
+			}
+			/**/
+			if ($wgUser->getRealName() == "") {
+				$personal_urls['userpage']['text'] .= ' (change "Real Name" in preferences) ';
+			} else {
+				$personal_urls['userpage']['text'] = $wgUser->getRealName();
+			}
+			unset($personal_urls['logout']);
+			/**/
+			$thisurl = $title->getPrefixedURL();
+			$personal_urls['fblogout'] = array('text' => wfMsg('fbconnectlogout'),
+			                                   'href' => Skin::makeSpecialUrl('Userlogout', $title->isSpecial('Preferences') ?
+			                                             '' : "returnto={$thisurl}"),
+			                                   'active' => false);
+			$personal_urls['fblink'] = array('text' => wfMsg('fbconnectlink'),
+			                                 'href' => 'http://www.facebook.com/profile.php?id=' . $wgUser->getName(),
+			                                 'active' => false);
+		}
+		
+		// Unset user talk page links
+		if (array_key_exists('mytalk', $personal_urls))
+			unset($personal_urls['mytalk']);
+
+		return true;
+	}
+	
+	/**
+	 * Modify the preferences form. At the moment, we simply turn the user name
+	 * into a link to the user's facebook profile.
+	 * 
+	 */
+	public static function RenderPreferencesForm($form, $output) {
+		global $wgUser;
+		$fb_uid = $wgUser->getName();
+		// If the user name is not a valid facebook ID (i.e. not a bunch of numbers) then we're done here
+		// TODO: I need a function that actually tests this
+		if ($fb_uid == "Admin") {
+			return true;
+		}
+		$html = $output->getHTML();
+		$i = strpos( $html, $fb_uid );
+		if ($i !== FALSE) {
+			// Replace the old output with the new output
+			$output->clearHTML();
+			$output->addHTML( substr($html, 0, $i) . preg_replace("($fb_uid)",
+			    "<a href='http://www.facebook.com/profile.php?id=$fb_uid'>$fb_uid</a>", substr($html, $i, -1), 1 ) );
+		}
+		return true;
+	}
+
 	/**
 	 * If the user isn't logged in, try to auto-authenticate via Facebook Connect
 	 */
@@ -112,162 +273,6 @@ class FBConnectHooks {
 		wfSetupSession();
 		$result = true;
 		
-		return true;
-	}
-
-	/**
-	 * Modify the preferences form. At the moment, we simply turn the user name
-	 * into a link to the user's facebook profile.
-	 * 
-	 */
-	public static function RenderPreferencesForm($form, $output) {
-		global $wgUser;
-		$fb_uid = $wgUser->getName();
-		// If the user name is not a valid facebook ID (i.e. not a bunch of numbers) then we're done here
-		// TODO: I need a function that actually tests this
-		if ($fb_uid == "Admin") {
-			return true;
-		}
-		$html = $output->getHTML();
-		$i = strpos( $html, $fb_uid );
-		if ($i !== FALSE) {
-			// Replace the old output with the new output
-			$output->clearHTML();
-			$output->addHTML( substr($html, 0, $i) . preg_replace("($fb_uid)",
-			    "<a href='http://www.facebook.com/profile.php?id=$fb_uid'>$fb_uid</a>", substr($html, $i, -1), 1 ) );
-		}
-		return true;
-	}
-
-	/**
-	 * Modify the user's persinal toolbar (in the upper right)
-	 */
-	static function PersonalUrls(&$personal_urls, &$title) {
-		global $wgUser, $wgLang, $wgOut, $wgFBConnectOnly;
-		wfLoadExtensionMessages('FBConnect');
-		$sk = $wgUser->getSkin();
-		
-		if ( !$wgUser->isLoggedIn() ) {
-			$returnto = ($title->getPrefixedUrl() == $wgLang->specialPage( 'Userlogout' )) ?
-			  '' : ('returnto=' . $title->getPrefixedURL());
-
-			$personal_urls['fbconnect'] = array('text' => wfMsg('fbconnectlogin'),
-			                                    #'href' => $sk->makeSpecialUrl( 'Userlogin', $returnto ),
-			                                    'href' => $sk->makeSpecialUrl( 'Connect', $returnto ),
-			                                    'active' => $title->isSpecial( 'Userlogin' ) );
-			if ($wgFBConnectOnly) {
-				# remove other personal toolbar links
-				foreach (array('login', 'anonlogin') as $k) {
-					if (array_key_exists($k, $personal_urls)) {
-						unset($personal_urls[$k]);
-					}
-				}
-			}
-		} else {
-			/* User's real name is not set at account creation. Why not? And why doesn't this workaround seem to work?
-			if ($wgUser->getRealName() == "") {
-				$wgAuth->updateUser($wgUser);
-			}
-			/**/
-			if ($wgUser->getRealName() == "") {
-				$personal_urls['userpage']['text'] .= ' (change "Real Name" in preferences) ';
-			} else {
-				$personal_urls['userpage']['text'] = $wgUser->getRealName();
-			}
-			unset($personal_urls['logout']);
-			/**/
-			$thisurl = $title->getPrefixedURL();
-			$personal_urls['fblogout'] = array('text' => wfMsg('fbconnectlogout'),
-			                                   'href' => Skin::makeSpecialUrl('Userlogout', $title->isSpecial('Preferences') ?
-			                                             '' : "returnto={$thisurl}"),
-			                                   'active' => false);
-			$personal_urls['fblink'] = array('text' => wfMsg('fbconnectlink'),
-			                                 'href' => 'http://www.facebook.com/profile.php?id=' . $wgUser->getName(),
-			                                 'active' => false);
-		}
-		
-		// Unset user talk page links
-		if (array_key_exists('mytalk', $personal_urls))
-			unset($personal_urls['mytalk']);
-
-		return true;
-	}
-	
-	/**
-	 * We seriously need to use a better hook... But which one allows injecting javascript src's into the page's body?
-	 * The dynamic source code loading [newElement("source") ...] technique didn't work for me.
-	 *
-	 */
-	static function ParserAfterTidy(&$parser, &$text) {
-		static $wgOnce = false;
-		//if (!isset($wgOnce) || !$wgOnce) {
-		if (!$wgOnce) {
-			$wgOnce = true;
-			self::SomeHookThatAllowsOneTimeRenderingToFooter($text);
-		}
-		return true;
-	}
-	
-	/**
-	 * Is there any hook for this task?
-	 *
-	 * Perhaps one of the skin hooks: SkinAfterBottomScripts, SkinAfterContent or SkinBuildSidebar...
-	 * Found one: SiteNoticeAfter
-	 */
-	static function SomeHookThatAllowsOneTimeRenderingToFooter(&$text) {
-		$text .= '<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php"></script>';
-		//$text .= '<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/XdCommReceiver.js" type="text/javascript"></script>';
-		//$text .= '<script src="/w/extensions/FBConnect/fbconnect.js"></script>';
-		return true;
-	}
-
-	/**
-	 * Injects some CSS and Javascript into the <head> of the page
-	 */
-	static function BeforePageDisplay(&$out, &$sk) {
-		global $wgTitle, $wgFBConnectLogoUrl, $wgScriptPath;
-		$thisurl = $wgTitle->getPrefixedURL();
-		
-		// Add a pretty Facebook logo in front of the userpage's link
-		$style = '<style type="text/css">
-			li#pt-userpage {
-				background: url(' . $wgFBConnectLogoUrl . ') top left no-repeat;
-			}
-		</style>';
-		
-		// Setup some variables and pseudo window.onload functions for Facebook Connect
-		$script = "";
-		$js_vars = array(
-			'api_key' => FBConnect::get_api_key(),
-			'already_logged_into_facebook' => FBConnect::getClient()->get_loggedin_user() ? "true" : "false",
-			'logout_url' => Skin::makeSpecialUrl('Userlogout', $wgTitle->isSpecial('Preferences') ? '' : "returnto={$thisurl}")
-		);
-		foreach( $js_vars as $name => $value ) {
-			if( $value == "true" || $value == "false" ) {
-				$script .= "var " . $name . " = " . $value . ";\n";
-			} else {
-				$script .= "var " . $name . " = '" . $value . "';\n";
-			}
-		}
-		// Onload functions from fbconnect.js (actually called in the <body> by addOnloadHook())
-		foreach( array( 'facebook_onload_addFBConnectButtons', 'facebook_init', 'facebook_onload' ) as $hook ) {
-			$script .= "addOnloadHook($hook);\n";
-		}
-		
-		// Styles and Scripts have been built, so add them to the page
-		if (isset($wgFBConnectLogoUrl) && $wgFBConnectLogoUrl) {
-			$out->addScript($style . "\n\t\t");
-		}
-		$out->addScript("<script src='$wgScriptPath/extensions/FBConnect/fbconnect.js'></script>");
-		$out->addInlineScript($script);
-		return true;
-	}
-	
-	static function ParserFirstCallInit(&$parser) {
-		$pHooks = FBConnectXFBML::availableTags();
-		foreach( $pHooks as $tag ) {
-			$parser->setHook( $tag, 'FBConnectXFBML::parserHook' );
-		}
 		return true;
 	}
 }
--- FBConnect/FBConnectXFBML.php
+++ FBConnect/FBConnectXFBML.php
@@ -34,22 +34,97 @@ class FBConnectXFBML {
 	/**
 	 * This function is the callback that the ParserFirstCallInit hook assigns
 	 * to the parser whenever a FBML tag is encountered (like <fb:name>).
-	 * Because every FBML tag directs to this function, we have no way of
-	 * knowing which tag was used.
+	 * Function createParserHook($tag) creates an anonymous (lambda-style)
+	 * function that simply redirects to parserHook(), filling in the missing
+	 * $tag argument with the $tag provided to createParserHook.
 	 */
-	static function parserHook($input, $args, &$parser ) {
-		return htmlspecialchars( $input );
+	static function parserHook($text, $args, &$parser, $tag = '' ) {
+		switch ($tag) {
+			case '':
+				return ''; // Error: We shouldn't be here!
+			// To implement a custom XFBML tag handler, simple case it here like so  
+			case 'fb:name':
+			default:
+				$attrs = "";
+				foreach( $args as $name => $value ) {
+					// TODO: !!!VERY IMPORTANT FOR SECURITY!!! strip all double-quotes from $value
+					$attrs .= " $name=\"$value\"";
+				}
+				return '<' . $tag . $attrs . '>' . $parser->recursiveTagParse($text) . "</$tag>";
+		}
 	}
 	
 	/**
-	 * Returns the availabe XFBML tags. For now, this is just an array copied
-	 * from <http://wiki.developers.facebook.com/index.php/XFBML>. Eventually,
-	 * this method can be replaced with one that gathers these tags from the
-	 * internet (e.g. by downloading an xml or js file) in real time.
+	 * Helper function for parserHook. Originally, all tags were directed to
+	 * this function, but I had no way of knowing whick tag provoked the function.
+	 */
+	static function createParserHook($tag) {
+		$args = '$text,$args,&$parser';
+		$code = 'return FBConnectXFBML::parserHook($text,$args,$parser,\''.$tag.'\');';
+		return create_function($args, $code);
+	}
+	
+	/**
+	 * Returns true if XFBML is enabled (i.e. $fbUseMarkup is not false).
+	 * Defaults to true if $fbUseMarkup is not set.
+	 */
+	static function isEnabled() {
+		global $fbUseMarkup;
+		return !isset($fbUseMarkup) || $fbUseMarkup;
+	}
+	
+	/**
+	 * Returns the availabe XFBML tags. For now, this is just an array copied from
+	 * <http://wiki.developers.facebook.com/index.php/XFBML> and the second-to-last line in
+	 * <http://static.ak.fbcdn.net/rsrc.php/zAE5U/lpkg/2netpns0/en_US/141/136684/js/connect.js.pkg.php>.
+	 * Eventually, this method can be replaced with one that gathers these tags
+	 * from the internet (e.g. by downloading an xml or js file) in real time.
+	 * 
+	 * This is necessary because the Facebook Platform is so new, and major updates
+	 * are being pushed out every week. With such turmoil regarding the available tags
+	 * and the features they offer, our list of tags should not be hardcoded into this file.
+	 * 
+	 * But for now... HELP! Where does Firefox pull in the XFBML tags in from??
 	 */
 	static function availableTags() {
-		$tags = array( 'sample' );
-		return $tags;
+		if (!self::isEnabled()) {
+			// If XFBML isn't enabled, then don't report any tags
+			return array( );
+		}
+		
+		// These are DEFINITELY valid tags (sarcasm intended -- see function doc comment)
+		$validTags = array('fb:container',
+		                   'fb:eventLink',
+		                   'fb:groupLink',
+		                   'fb:login-button',
+		                   'fb:name',
+		                   'fb:photo',
+		                   'fb:profile-pic',
+		                   'fb:prompt-permission',
+		                   'fb:pronoun',
+		                   'fb:serverFbml',
+		                   'fb:unconnected-friends-count',
+		                   'fb:user-status');
+		// This tag is listed in the facebook dev wiki's documentation, but I couldn't
+		// find any mention of it in the JavaScript package connect.js.pkg.php
+		// Is it valid? What's going on in this JavaScript file?
+		$wikiTags =  array('fb:connect-form');
+		// I found these in the JavaScript package connect.js.pkg.php, but the wiki
+		// makes no mention of them. Are they valid?
+		$jsTags =    array('fb:add-section-button',
+		                   'fb:share-button',
+		                   'fb:userLink',
+		                   'fb:video');
+		// Oh well, include them anyway
+		return array_merge( $validTags, $wikiTags, $jsTags );
 	}
 }
 	
+
+
+
+
+
+
+
+
