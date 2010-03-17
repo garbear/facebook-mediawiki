e84c477e7567064d4169a588974727c902efa5fb
garbear <garbearucla@gmail.com>
2010-03-17 16:16:41 +0000

    Architectural modification - wiki usernames for Facebook Connect accounts are now associated with a Facebook UID in the database.
    fbconnect_table.sql - MySQL table definition
    fbconnect_table.pg.sql - Postgres table definition
    FBConnectDB.php - Static class to manipulate the new table
    
    Coming soon: added hook "LoadExtensionSchemaUpdates" to automatically insert tables when maintenance/update.php is run

---
+++ FBConnect/FBConnectDB.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Copyright © 2010 Garrett Brown <http://www.mediawiki.org/wiki/User:Gbruin>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * Not a valid entry point, skip unless MEDIAWIKI is defined.
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+}
+
+
+/**
+ * Class for manipulating the custom users_fbconnect table.
+ */
+class FBConnectDB {
+	/**
+	 * Find the Facebook ID of the given user, if any.
+	 */
+	public static function getFacebookIDs( $user ) {
+		$fbid = array();
+
+		if ( $user instanceof User && $user->getId() != 0 ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$res = $dbr->select(
+				array( 'user_fbconnect' ),
+				array( 'user_fbid' ),
+				array( 'user_id' => $user->getId() ),
+				__METHOD__
+			);
+
+			foreach( $res as $row ) {
+				$fbid[] = $row->user_fbid;
+			}
+			$res->free();
+		}
+		return $fbid;
+	}
+	
+	/**
+	 * Find the user by their Facebook ID.
+	 */
+	public static function getUser( $fbid ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$id = $dbr->selectField(
+			'user_fbconnect',
+			'user_id',
+			array( 'user_fbid' => $fbid ),
+			__METHOD__
+		);
+		if ( $id ) {
+			return User::newFromId( $id );
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Add a User <-> Facebook ID association to the database.
+	 */
+	public static function addFacebookID( $user, $fbid ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->insert(
+			'user_fbconnect',
+			array(
+				'user_id' => $user->getId(),
+				'user_fbid' => $fbid
+			),
+			__METHOD__
+		);
+	}
+	
+	/**
+	 * Remove a User <-> Facebook ID association from the database.
+	 */
+	public static function removeFacebookID( $user, $fbid ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->delete(
+			'user_fbconnect',
+			array(
+				'user_id' => $user->getId(),
+				'user_fbid' => $fbid
+			),
+			__METHOD__
+		);
+		return (bool)$dbw->affectedRows();
+	}
+}
