6f4c16a25d4c5d8a578e639018db2061afdaac4b
garbear <garbearucla@gmail.com>
2010-03-17 16:12:35 +0000

    Simplify xd_receiver.php (don't read from config.php anymore).

--- FBConnect/xd_receiver.php
+++ FBConnect/xd_receiver.php
@@ -1,37 +1,26 @@
 <?php
 
-$dir = dirname(__FILE__) . '/';
-if (file_exists($dir . 'config.php')) {
-	include_once							$dir . 'config.php';
-} else {
-	include_once							$dir . 'config.sample.php';
-}
-
-// cache the xd_receiver
+// Cache the xd_receiver.php file
 header('Cache-Control: max-age=225065900');
 header('Expires:');
 header('Pragma:');
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head>
-    <title>Cross Domain Receiver Page</title>
+	<title>Cross Domain Receiver Page</title>
 </head>
 <body>
-
-<!--
-This is a cross domain (XD) receiver page. It needs to be placed on your domain so that the Javascript
-  library can communicate within the iframe permission model. Put it here:
-
-  http://www.example.com/path_to_extensions/FBConnect/xd_receiver.php
--->
-
 <?php
-  echo '<script src="http://static.ak.' . (isset($fbBaseURL) ? $fbBaseURL : "connect.facebook.com" ) .
-       '/js/api_lib/v0.4/XdCommReceiver.debug.js" type="text/javascript"></script>';
+/*
+ * This is a cross domain (XD) receiver page. It needs to be placed on your
+ * domain so that the Javascript library can communicate within the iframe
+ * permission model. Put it here:
+ * 
+ * http://www.example.com/path_to_extensions/FBConnect/xd_receiver.php
+ */
 ?>
+	<script src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/XdCommReceiver.js" type="text/javascript"></script>
 </body>
 </html>
