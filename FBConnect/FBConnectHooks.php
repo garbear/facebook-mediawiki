be6f7cdc52045b7597d591e8bc36bed45bc780a6
garbear <garbearucla@gmail.com>
2010-09-30 06:06:36 +0000

    XHTML validation: required attribute "type" not specified

--- FBConnect/FBConnectHooks.php
+++ FBConnect/FBConnectHooks.php
@@ -107,7 +107,7 @@ class FBConnectHooks {
 		if ( !empty($wgFbScript) && empty($wgNoExternals) ) {
 			$out->prependHTML('
 				<div id="fb-root"></div>
-				<script>
+				<script type="text/javascript">
 					(function(){var e=document.createElement("script");e.type="' .
 					$wgJsMimeType . '";e.src="' . $wgFbScript .
 					'";e.async=true;document.getElementById("fb-root").appendChild(e)})();
