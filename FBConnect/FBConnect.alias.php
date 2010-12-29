16cc0d6c7a4285fedd6e31ed89bafa01753c5f79
garbear <garbearucla@gmail.com>
2010-12-29 00:18:04 +0000

    Localisation updates from translatewiki.net
    (cherry picked from http://svn.wikimedia.org/viewvc/mediawiki?view=revision&revision=75726)

--- FBConnect/FBConnect.alias.php
+++ FBConnect/FBConnect.alias.php
@@ -13,6 +13,26 @@ $specialPageAliases['en'] = array(
 	'Connect' => array( 'Connect', 'ConnectAccount' ),
 );
 
+/** Spanish (Español) */
+$specialPageAliases['es'] = array(
+	'Connect' => array( 'Conectar', 'ConectarCuenta' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'Connect' => array( '接続' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'Connect' => array( 'ബന്ധിപ്പിക്കുക', 'അംഗത്വംബന്ധിപ്പിക്കുക' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'Connect' => array( 'Verbinden', 'GebruikerVerbinden' ),
+);
+
 /**
  * For backwards compatibility with MediaWiki 1.15 and earlier.
  */
