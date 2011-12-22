31a909d31d6ee0d9940f5fefeb719e6351334a81
Garrett Brown <garbearucla@gmail.com>
2011-12-21 22:14:04 -0800

    Cosmetic: sort the locales

--- Facebook/FacebookLanguage.php
+++ Facebook/FacebookLanguage.php
@@ -23,14 +23,14 @@ class FacebookLanguage{
 
 	// All of the Facebook Locales according to http://www.facebook.com/translations/FacebookLocales.xml as of 20100622
 	private static $allFbLocales = array(
-		'ca_ES', 'cs_CZ', 'cy_GB', 'da_DK', 'de_DE', 'eu_ES', 'en_PI', 'en_UD', 'ck_US', 'en_US', 'es_LA', 'es_CL', 'es_CO', 'es_ES', 'es_MX',
-		'es_VE', 'fb_FI', 'fi_FI', 'fr_FR', 'gl_ES', 'hu_HU', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nn_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT',
-		'ro_RO', 'ru_RU', 'sk_SK', 'sl_SI', 'sv_SE', 'th_TH', 'tr_TR', 'ku_TR', 'zh_CN', 'zh_HK', 'zh_TW', 'fb_LT', 'af_ZA', 'sq_AL', 'hy_AM',
-		'az_AZ', 'be_BY', 'bn_IN', 'bs_BA', 'bg_BG', 'hr_HR', 'nl_BE', 'en_GB', 'eo_EO', 'et_EE', 'fo_FO', 'fr_CA', 'ka_GE', 'el_GR', 'gu_IN',
-		'hi_IN', 'is_IS', 'id_ID', 'ga_IE', 'jv_ID', 'kn_IN', 'kk_KZ', 'la_VA', 'lv_LV', 'li_NL', 'lt_LT', 'mk_MK', 'mg_MG', 'ms_MY', 'mt_MT',
-		'mr_IN', 'mn_MN', 'ne_NP', 'pa_IN', 'rm_CH', 'sa_IN', 'sr_RS', 'so_SO', 'sw_KE', 'tl_PH', 'ta_IN', 'tt_RU', 'te_IN', 'ml_IN', 'uk_UA',
-		'uz_UZ', 'vi_VN', 'xh_ZA', 'zu_ZA', 'km_KH', 'tg_TJ', 'ar_AR', 'he_IL', 'ur_PK', 'fa_IR', 'sy_SY', 'yi_DE', 'gn_PY', 'qu_PE', 'ay_BO',
-		'se_NO', 'ps_AF', 'tl_ST'
+		'af_ZA', 'ar_AR', 'ay_BO', 'az_AZ', 'be_BY', 'bg_BG', 'bn_IN', 'bs_BA', 'ca_ES', 'ck_US', 'cs_CZ', 'cy_GB', 'da_DK', 'de_DE', 'el_GR',
+		'en_GB', 'en_PI', 'en_UD', 'en_US', 'eo_EO', 'es_CL', 'es_CO', 'es_ES', 'es_LA', 'es_MX', 'es_VE', 'et_EE', 'eu_ES', 'fa_IR', 'fb_FI',
+		'fb_LT', 'fi_FI', 'fo_FO', 'fr_CA', 'fr_FR', 'ga_IE', 'gl_ES', 'gn_PY', 'gu_IN', 'he_IL', 'hi_IN', 'hr_HR', 'hu_HU', 'hy_AM', 'id_ID',
+		'is_IS', 'it_IT', 'ja_JP', 'jv_ID', 'ka_GE', 'kk_KZ', 'km_KH', 'kn_IN', 'ko_KR', 'ku_TR', 'la_VA', 'li_NL', 'lt_LT', 'lv_LV', 'mg_MG',
+		'mk_MK', 'ml_IN', 'mn_MN', 'mr_IN', 'ms_MY', 'mt_MT', 'nb_NO', 'ne_NP', 'nl_BE', 'nl_NL', 'nn_NO', 'pa_IN', 'pl_PL', 'ps_AF', 'pt_BR',
+		'pt_PT', 'qu_PE', 'rm_CH', 'ro_RO', 'ru_RU', 'sa_IN', 'se_NO', 'sk_SK', 'sl_SI', 'so_SO', 'sq_AL', 'sr_RS', 'sv_SE', 'sw_KE', 'sy_SY',
+		'ta_IN', 'te_IN', 'tg_TJ', 'th_TH', 'tl_PH', 'tl_ST', 'tr_TR', 'tt_RU', 'uk_UA', 'ur_PK', 'uz_UZ', 'vi_VN', 'xh_ZA', 'yi_DE', 'zh_CN',
+		'zh_HK', 'zh_TW', 'zu_ZA'
 	);
 	
 	private static $messageKey = 'facebook-mediawiki-lang-to-fb-locale';
