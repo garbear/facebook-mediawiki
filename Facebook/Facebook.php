90de6331f7e948f67796683d2adf3608bb8ed427
Garrett Brown <garbearucla@gmail.com>
2011-12-26 01:56:35 -0800

    Bump version string to 4.0-alpha

--- Facebook/Facebook.php
+++ Facebook/Facebook.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright © 2008-2010 Garrett Brown <http://www.mediawiki.org/wiki/User:Gbruin>
+ * Copyright © 2008-2012 Garrett Brown <http://www.mediawiki.org/wiki/User:Gbruin>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -27,7 +27,7 @@
  * @file
  * @ingroup Extensions
  * @author Garrett Brown, Sean Colombo
- * @copyright Copyright © 2010 Garrett Brown, Sean Colombo
+ * @copyright Copyright © 2008-2012 Garrett Brown, Sean Colombo
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
@@ -43,7 +43,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgEnablePreferencesExt = true;
 
 // Facebook version
-define( 'MEDIAWIKI_FACEBOOK_VERSION', '3.1-GIT, December 21, 2011' );
+define( 'MEDIAWIKI_FACEBOOK_VERSION', '4.0-alpha, December 26, 2011' );
 
 // Magic string to use in substitution (must be defined prior to including config.php).
 define( 'FACEBOOK_LOCALE', '%LOCALE%');
