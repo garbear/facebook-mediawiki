garbear/facebook-mediawiki

commit 1839a40d73918a34cda4b45e1ef1bf6aa9ed0a5b

Changed: Rename $wgFbConvertButton to $wgFbAlwaysShowLogin

Facebook/config.default.php
@@ -89,13 +89,13 @@ $wgFbSocialPlugins = true;
  * Shows the real name for all Facebook users in the personal toolbar (in the
  * upper right) instead of their wiki username.
  */
-$wgFbUseRealName = false;
+$wgFbUseRealName = true;
 
 /**
- * Another personal toolbar option: Show a button to log in with Facebook to
- * users who have not connected their username with a Facebook account yet.
+ * Another personal toolbar option: Always show a button to log in to Facebook.
+ * By default, this button is only show to anonymous users.
  */
-$wgFbConvertButton = true;
+$wgFbAlwaysShowLogin = true;
 
 /**
  * The Facebook icon. You can copy this image to your server if you want, or

Facebook/FacebookHooks.php
@@ -279,62 +279,53 @@ STYLE;
 	 * Modify the user's persinal toolbar (in the upper right).
 	 */
 	public static function PersonalUrls( &$personal_urls, &$wgTitle ) {
-		global $wgUser;
+		global $wgUser, $facebook, $wgFbUseRealName, $wgFbAlwaysShowLogin, $wgFbDisableLogin;
 		wfLoadExtensionMessages('Facebook');
 		
-		$fbUser = new FacebookUser();
-		if ( $wgUser->isLoggedIn() ) {
-			if ( count( FacebookDB::getFacebookIDs($wgUser) ) > 0 ) {
-				// The user is logged in and connected
-				global $wgFbUseRealName;
-				if ( !empty( $wgFbUseRealName ) ) {
-					// Start with the real name in the database
-					$name = $wgUser->getRealName();
-					if ( empty( $name ) ) {
-						// Ask Facebook for the real name
-						$name = $fbUser->getUserInfo('name');
-					}
-					// Make sure we were able to get a name from the database or Facebook
-					if ( !empty( $name ) ) {
-						$personal_urls['userpage']['text'] = $name;
-					}
+		if ( $wgUser->isLoggedIn() && !empty( $wgFbUseRealName ) ) {
+			$fb_ids = FacebookDB::getFacebookIDs($wgUser);
+			if ( count( $fb_ids ) ) {
+				// Start with the real name in the database
+				$name = $wgUser->getRealName();
+				if ( empty( $name ) ) {
+					// Ask Facebook for the real name
+					$fbUser = new FacebookUser($fb_ids[0]);
+					$name = $fbUser->getUserInfo('name');
 				}
-			} else {
-				// User is logged in but not connected
-				global $wgFbConvertButton;
-				if ( !empty( $wgFbConvertButton ) ) {
-					// Place the convert button before the logout link
-					$logout_key = end( array_keys( $personal_urls ) );
-					$logout_item = end( $personal_urls );
-					$personal_urls = array_slice($personal_urls, 0, -1, true);
-					$personal_urls['facebook'] = array(
-						'text'   => wfMsg( 'facebook-connect' ),
-						'href'   => '#',
-						'class'  => 'mw-facebook-logo',
-						'active' => $wgTitle->isSpecial( 'Connect' ),
-					);
-					$personal_urls[$logout_key] = $logout_item;
-				}
-			}
-		} else {
-			// The user is not logged in
-			// Add an option to connect via Facebook Connect
-			$personal_urls['facebook'] = array(
-				'text'   => wfMsg( 'facebook-connect' ),
-				'href'   => '#', # SpecialPage::getTitleFor('Connect')->getLocalUrl('returnto=' . $wgTitle->getPrefixedURL()),
-				'class' => 'mw-facebook-logo',
-				'active' => $wgTitle->isSpecial('Connect'),
-			);
-			global $wgFbDisableLogin;
-			if ( !empty( $wgFbDisableLogin ) ) {
-				// Remove other personal toolbar links
-				foreach (array('login', 'anonlogin') as $k) {
-					if (array_key_exists($k, $personal_urls)) {
-						unset($personal_urls[$k]);
-					}
+				// Make sure we were able to get a name from the database or Facebook
+				if ( !empty( $name ) ) {
+					$personal_urls['userpage']['text'] = $name;
 				}
 			}
 		}
+		
+		if (!empty( $wgFbAlwaysShowLogin ) && (!$wgUser->isLoggedIn() || !$facebook->getUser() ||
+				!in_array($facebook->getUser(), FacebookDB::getFacebookIDs($wgUser)))) {
+			if ( isset( $personal_urls['logout'] ) ) {
+				// Place the convert button before the logout link
+				$logout_item = end( $personal_urls );
+				$personal_urls = array_slice( $personal_urls, 0, -1, true );
+			}
+			$personal_urls['facebook'] = array(
+				'text'   => wfMsg( 'facebook-connect' ),
+				'href'   => '#', # SpecialPage::getTitleFor('Connect')->getLocalUrl('returnto=' . $wgTitle->getPrefixedURL()),
+				'class'  => 'mw-facebook-logo',
+				'active' => $wgTitle->isSpecial( 'Connect' ),
+			);
+			if ( !empty( $logout_item ) ) {
+				$personal_urls['logout'] = $logout_item;
+			}
+		}
+		
+		if ( !$wgUser->isLoggedIn() && !empty( $wgFbDisableLogin ) ) {
+			// Remove other personal toolbar links
+			foreach (array('login', 'anonlogin') as $k) {
+				if (array_key_exists($k, $personal_urls)) {
+					unset($personal_urls[$k]);
+				}
+			}
+		}
+		
 		return true;
 	}
 	

commit f82d9d5e93cf755e54170991eb5f675249aaaa96

Cosmetic: Update documentation for $wgFbAlwaysShowLogin
$wgFbAlwaysShowLogin is now defaulted to false (this used to be the case but 1839a40 accidentally changed this)

Facebook/config.default.php
@@ -92,10 +92,15 @@ $wgFbSocialPlugins = true;
 $wgFbUseRealName = true;
 
 /**
- * Another personal toolbar option: Always show a button to log in to Facebook.
- * By default, this button is only show to anonymous users.
+ * Another personal toolbar option: always show a button to log in with
+ * Facebook. By default, this button is only shown to anonymous users.
+ * 
+ * This button simply links to Special:Connect. If you would like to let users
+ * convert their usernames to Facebook-enabled accounts, consider linking to
+ * Special:Connect from the Main Page instead of showing this button on every
+ * page.
  */
-$wgFbAlwaysShowLogin = true;
+$wgFbAlwaysShowLogin = false;
 
 /**
  * The Facebook icon. You can copy this image to your server if you want, or