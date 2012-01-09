f9aaea040fba6888387e2ee19cc9b1c9532ef490
Garrett Brown <garbearucla@gmail.com>
2012-01-09 07:23:47 -0800

    [Version] Bump the version string

--- Facebook/Facebook.php
+++ Facebook/Facebook.php
@@ -40,7 +40,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 // Facebook version
-define( 'MEDIAWIKI_FACEBOOK_VERSION', '4.0-beta, January 8, 2012' );
+define( 'MEDIAWIKI_FACEBOOK_VERSION', '4.0-beta, January 9, 2012' );
 
 // Magic string to use in substitution (must be defined prior to including config.php).
 define( 'FACEBOOK_LOCALE', '%LOCALE%');
