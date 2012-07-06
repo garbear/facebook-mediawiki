e251a6b4573ac9d1f5be1b45d3f73a42dadc2d92
Garrett Brown <garbearucla@gmail.com>
2012-07-06 09:56:34 -0700

    [Version] Bump version number for fixes introduced in 1bce55c and ce46cad

--- Facebook/Facebook.php
+++ Facebook/Facebook.php
@@ -40,7 +40,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 // Facebook version
-define( 'MEDIAWIKI_FACEBOOK_VERSION', '4.0.3-stable, April 9, 2012' );
+define( 'MEDIAWIKI_FACEBOOK_VERSION', '4.0.4-stable, April 9, 2012' );
 
 // Magic string to use in substitution (must be defined prior to including config.php).
 define( 'FACEBOOK_LOCALE', '%LOCALE%');
